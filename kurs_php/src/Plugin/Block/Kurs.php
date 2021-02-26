1e57fba8f2339974bab5836e96fb1c239b199a14
tobikuhn <68896373+tobikuhn@users.noreply.github.com>
2021-02-26 10:44:41 +0100

    changed name (#4)

--- kurs_php/src/Plugin/Block/Kurs.php
+++ kurs_php/src/Plugin/Block/Kurs.php
@@ -5,12 +5,12 @@ namespace Drupal\kurs_php\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 
 /**
- * Provides a 'Hello' Block.
+ * Provides a 'Kurs' Block.
  *
  * @Block(
- *   id = "hello_block",
- *   admin_label = @Translation("Hello block"),
- *   category = @Translation("Hello World"),
+ *   id = "kurs_block",
+ *   admin_label = @Translation("Kurs block"),
+ *   category = @Translation("Kurs"),
  * )
  */
 class Kurs extends BlockBase {
