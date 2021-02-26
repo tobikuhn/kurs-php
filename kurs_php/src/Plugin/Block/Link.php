f1253f1b95c8df925e3b6cded20caa53e60343ef
tobikuhn <68896373+tobikuhn@users.noreply.github.com>
2021-02-26 10:32:45 +0100

    2.step (#2)
    * added readme
    
    * added Kurs.php
    
    * Added Kurs.php
    Last commit was Link.php

---
+++ kurs_php/src/Plugin/Block/Link.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\kurs_php\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "link",
+ *   admin_label = @Translation("Link"),
+ *   category = @Translation("Hello World"),
+ * )
+ */
+class Link extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+	$now = date("Y-m-d-H:i:s");
+	$link1 = "/kurs?time=$now";
+	$t= "Aktueller Kurs: 0,50<br><a href=\"".$link1."\">Zum Verlauf</a>";
+    return [
+      '#markup' => $t,
+    ];
+  }
+  
+
+}
+
